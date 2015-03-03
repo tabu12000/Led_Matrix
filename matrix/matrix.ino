fb112cc7714cb19ccbd87fe574ef8af848559583
tabu12000 <tabu12346@gmail.com>
2015-03-03 18:09:21 -0500

    Added Shift Register
    Added function for shift register. Also changed the name of the main
    file to 'matrix'.  Organized the file so it is less of a mess.

--- matrix/matrix.ino
+++ matrix/matrix.ino
@@ -1,13 +1,46 @@
 #include <StandardCplusplus.h>
 #include <vector>
-
 #include <Char2Array.h>
-
 #include <Arduino.h>
-
 #include "EEPROM.h"
 #include "DB.h"
 
+
+Char2Array converter;
+
+
+
+const int data = 4;
+const int clock = 5;
+const int latch = 6;
+const int blank = 7;
+
+const int button_pin = 3;
+const int led_pin = 12;
+
+const int total_shifts = 6;
+int shift_count = 0;
+
+void shift(){
+  if(shift_count > total_shifts - 2){
+    digitalWrite(data, HIGH);
+    digitalWrite(clock, HIGH);
+    digitalWrite(data, LOW);
+    digitalWrite(clock, LOW);
+    
+    shift_count = 0;
+  } 
+  else{
+    digitalWrite(clock, HIGH);
+    digitalWrite(clock, LOW);
+    
+    shift_count += 1;
+  }
+  
+  digitalWrite(latch, HIGH);
+  digitalWrite(latch, LOW);
+  
+}
 
 //Gets the next complete input from serial
 //All user input must end with >, known as "end of message" character
@@ -33,12 +66,10 @@ String wait_for_next(){
     return temp;
 }
 
-Char2Array converter;
 
-int led_pin = 12;
-int button_pin = 3;
 
 void admin_mode(){
+  Serial.begin(9600);
   
    while(Serial.available() == 0){
        Serial.println("Welcome to admin mode. Enter any input to begin. Remember to end all input with '>'.");
@@ -54,6 +85,8 @@ void admin_mode(){
    while(wait_for_next() != "exit"){
      Serial.println("Enter 'exit' to exit");}
      digitalWrite(led_pin, LOW);
+     
+  Serial.end();
 }
 
 void check_for_button(){
@@ -73,16 +106,21 @@ void check_for_button(){
 }
 
 void setup(){
-   Serial.begin(9600); 
    pinMode(led_pin, OUTPUT);
    pinMode(button_pin, INPUT);
-   check_for_button();
+   
+   pinMode(latch, OUTPUT);
+   pinMode(data, OUTPUT);
+   pinMode(clock, OUTPUT);
+   pinMode(blank, OUTPUT);
+   
+   //check_for_button();
  }
 
 
 void loop(){
-   String thing = wait_for_next();
-   Serial.println(thing);
+   shift();
+   delay(250);
    
    
   
